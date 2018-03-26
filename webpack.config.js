af3f5a6e45c5b8056f1a2562ed7a01927636e28c
Icebob <mereg.norbert@gmail.com>
2018-03-26 21:49:06 +0200

    remove unused packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 
-// const version = require("./package.json").version;
-// const banner = "/**\n" + " * moleculer-ws-client v" + version + "\n" + " * https://github.com/ColonelBundy/moleculer-ws-client\n" + " * Released under the MIT License.\n" + " */\n";
-
-const Visualizer = require('webpack-visualizer-plugin');
+//const Visualizer = require('webpack-visualizer-plugin');
 
 module.exports = {
 	entry: './src/index.ts',
@@ -31,11 +28,6 @@ module.exports = {
 
 	plugins: [
 		new webpack.IgnorePlugin(/^ws$/),
-		// new webpack.BannerPlugin({
-		// 	banner,
-		// 	raw: true
-		// }),
-
-		new Visualizer()
+		//new Visualizer()
 	]
 };
