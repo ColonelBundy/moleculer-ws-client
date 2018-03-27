2e218d0d374cb5094cd252e66f87d50ac695a69f
ColonelBundy <colonelbundy@gmail.com>
2018-03-27 17:21:20 +0200

    fixed webpack example

--- examples/server.js
+++
@@ -1,8 +0,0 @@
-const WebSocketServer = require('ws').Server,
-Server = new WebSocketServer({port: 3000})
-
-Server.on('connection', function (ws) {
-  ws.on('message', function (message) {
-    console.log('received: %s', message)
-  })
-})
--- webpack.config.js
+++ webpack.config.js
@@ -4,39 +4,41 @@ const webpack = require('webpack');
 //const Visualizer = require('webpack-visualizer-plugin');
 
 module.exports = {
-	entry: './src/index.ts',
-	mode: 'production',
+  entry: './src/index.ts',
+  mode: 'production',
 
-	module: {
-		rules: [{
-			test: /\.tsx?$/,
-			use: [
-				{
-					loader: 'ts-loader',
-					options: {
-						compilerOptions: {
-							"outDir": ".",
-						}
-					}
-				}
-			],
-			exclude: /node_modules/,
-		}]
-	},
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              compilerOptions: {
+                outDir: '.'
+              }
+            }
+          }
+        ],
+        exclude: /node_modules/
+      }
+    ]
+  },
 
-	resolve: {
-		extensions: ['.tsx', '.ts', '.js']
-	},
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
 
-	output: {
-		filename: 'moleculer-ws-client.js',
-		path: path.resolve(__dirname, 'dist'),
-		library: "MoleculerWs",
-		libraryTarget: "umd"
-	},
+  output: {
+    filename: 'moleculer-ws-client.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: 'MoleculerWs',
+    libraryTarget: 'umd'
+  },
 
-	plugins: [
-		new webpack.IgnorePlugin(/^ws$/),
-		//new Visualizer()
-	]
+  plugins: [
+    new webpack.IgnorePlugin(/^ws$/)
+    //new Visualizer()
+  ]
 };
